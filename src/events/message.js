956fb173bfd1f8dcda95f8830437042c5ead6a5c
Victor <33154756+Victor2106@users.noreply.github.com>
2020-08-02 16:54:26 +0200

    Optimize event

--- src/events/message.js
+++ src/events/message.js
@@ -1,11 +1,9 @@
 "use strict";
 
-module.exports = async function (client, message) {
-    if (!message.guild || message.author.bot) { return; }
-    if (!message.channel.permissionsFor(client.user.id).has("SEND_MESSAGES")) return;
-    if (!message.content.startsWith(client.config.bot.prefix)) return;
+module.exports = function (client, message) {
+    if (!message.guild || message.author.bot || !message.channel.permissionsFor(client.user.id).has("SEND_MESSAGES") || !message.content.startsWith(client.config.bot.prefix)) return;
 
-    if (client.uptime < 60000) return message.channel.send(`⚠ ${client.user.username} is starting up, please be patient`);
+    //if (client.uptime < 60000) return message.channel.send(`⚠ ${client.user.username} is starting up, please be patient`);
 
     const arg = message.content.trim().slice(client.config.bot.prefix.length).split(/ +/g);
     const command = client.commands.get(arg[0]) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(arg[0]));
@@ -16,11 +14,11 @@ module.exports = async function (client, message) {
     const check = client.cooldown.get(message.author.id, command.name);
 
     if (check.status) {
-        await command.run(client, message, args);
+        command.run(client, message, args);
         console.log(`[Commands] ${command.name} | Auteur: ${message.author.tag} | Server: ${message.guild === null ? 'DM' : message.guild.name} | Hour: ${new Date()}`);
 
         if (command.cooldown > 0) client.cooldown.add(message.author.id, command.name, command.cooldown);
     } else {
-        message.channel.send(`You have \`${Math.round(check.message / 1000)}\` seconds before you can repeat this command`);
+        message.channel.send(`You have \`${Math.round(check.time / 1000)}\` seconds before you can repeat this command`);
     }
 };
